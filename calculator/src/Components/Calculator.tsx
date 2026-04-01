12f99e3213e48672e250c4c037920922c16e4c75
DIVINEakisa <akisadivine11@gmail.com>
2026-04-01 19:00:56 +0200

    Add the tracking Function

--- calculator/src/Components/Calculator.tsx
+++ calculator/src/Components/Calculator.tsx
@@ -25,38 +25,93 @@ export default function Calculator() {
 
   const [num1, setNum1] = useState(0);
   const [num2, setNum2] = useState(0);
-  const [state, action] = useReducer(reducer, []);
+  const [operator, setOperator] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 0);
 
   return (
     <main className="bg-[#feebe7] rounded-3xl p-5 w-[300px] mx-auto mt-5 font-bold text-2xl">
-      {/* Display */}
-      <div className="bg-gray-400 col-span-4 p-4 mb-2 text-right">0</div>
+      <div className="bg-gray-400 col-span-4 p-4 mb-2 text-right">{num1}</div>
 
-      {/* Grid Buttons */}
       <div className="grid grid-cols-4">
-        <div className="p-4 text-center" onClick={()}>AC {state}</div>
-        <div className="p-4 text-center">+/-</div>
-        <div className="p-4 text-center">%</div>
-        <div className="p-4 text-center bg-[#f54927] text-white">:</div>
+        <div className="p-4 text-center" onClick={() => handleClick("AC")}>
+          AC
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("+/-")}>
+          +/-
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("%")}>
+          %
+        </div>
+        <div
+          className="p-4 text-center bg-[#f54927] text-white"
+          onClick={() => handleClick(":")}
+        >
+          :
+        </div>
 
-        <div className="p-4 text-center">7</div>
-        <div className="p-4 text-center">8</div>
-        <div className="p-4 text-center">9</div>
-        <div className="p-4 text-center bg-[#f54927] text-white">X</div>
+        <div className="p-4 text-center" onClick={() => handleClick("7")}>
+          7
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("8")}>
+          8
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("9")}>
+          9
+        </div>
+        <div
+          className="p-4 text-center bg-[#f54927] text-white"
+          onClick={() => handleClick("*")}
+        >
+          X
+        </div>
 
-        <div className="p-4 text-center">4</div>
-        <div className="p-4 text-center">5</div>
-        <div className="p-4 text-center">6</div>
-        <div className="p-4 text-center bg-[#f54927] text-white">-</div>
+        <div className="p-4 text-center" onClick={() => handleClick("4")}>
+          4
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("5")}>
+          5
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("6")}>
+          6
+        </div>
+        <div
+          className="p-4 text-center bg-[#f54927] text-white"
+          onClick={() => handleClick("-")}
+        >
+          -
+        </div>
 
-        <div className="p-4 text-center">1</div>
-        <div className="p-4 text-center">2</div>
-        <div className="p-4 text-center">3</div>
-        <div className="p-4 text-center bg-[#f54927] text-white">+</div>
+        <div className="p-4 text-center" onClick={() => handleClick("1")}>
+          1
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("2")}>
+          2
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick("3")}>
+          3
+        </div>
+        <div
+          className="p-4 text-center bg-[#f54927] text-white"
+          onClick={() => handleClick("+")}
+        >
+          +
+        </div>
 
-        <div className="p-4 text-center col-span-2 ">0</div>
-        <div className="p-4 text-center">.</div>
-        <div className="p-4 text-center bg-[#f54927] text-white">=</div>
+        <div
+          className="p-4 text-center col-span-2 "
+          onClick={() => handleClick("0")}
+        >
+          0
+        </div>
+        <div className="p-4 text-center" onClick={() => handleClick(".")}>
+          .
+        </div>
+        <div
+          className="p-4 text-center bg-[#f54927] text-white"
+          onClick={() => handleClick("=")}
+        >
+          =
+        </div>
       </div>
     </main>
   );
